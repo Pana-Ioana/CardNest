9b174c96bf48b966af6bdad86b06d920f6adc869
ioana <ioanapana2003@gmail.com>
2025-12-11 19:42:48 +0200

    feat: add middleware for JSON parsing, CORS, and logging

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1,6 +1,12 @@
-import express, { json } from 'express';
+import express, {json} from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'The server is up and running!',
